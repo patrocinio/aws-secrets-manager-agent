640de7653cbfac8d0fa078bee1994be58f253461
Eduardo Patrocinio <epatro@amazon.com>
2024-02-03 17:44:34 -0500

    Use SecretCache

--- agent.go
+++ agent.go
@@ -1,45 +1,26 @@
-// Use this code snippet in your app.
-// If you need more information about configurations or implementing the sample code, visit the AWS docs:
-// https://aws.github.io/aws-sdk-go-v2/docs/getting-started/
 package main
 
 import (
-	"context"
 	"fmt"
-	"log"
+
 	"net/http"
 
-	"github.com/aws/aws-sdk-go-v2/aws"
-	"github.com/aws/aws-sdk-go-v2/config"
-	"github.com/aws/aws-sdk-go-v2/service/secretsmanager"
-
+	"github.com/aws/aws-secretsmanager-caching-go/secretcache"
 	"github.com/gin-gonic/gin"
 )
 
-var svc *secretsmanager.Client
+var secretCache *secretcache.Cache
 
 func getSecret(c *gin.Context) {
 	secretName := c.Query("secretId")
 	fmt.Printf("secretName: %s\n", secretName)
 
-	input := &secretsmanager.GetSecretValueInput{
-		SecretId:     aws.String(secretName),
-		VersionStage: aws.String("AWSCURRENT"), // VersionStage defaults to AWSCURRENT if unspecified
-	}
+	result, _ := secretCache.GetSecretString(secretName)
 
-	result, err := svc.GetSecretValue(context.TODO(), input)
-	if err != nil {
-		// For a list of exceptions thrown, see
-		// https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
-		log.Fatal(err.Error())
-	}
+	fmt.Printf("result: %s\n", result)
 
-	// Decrypts secret using the associated KMS key.
-	var secretString string = *result.SecretString
+	c.IndentedJSON(http.StatusOK, result)
 
-	fmt.Printf("Secret: %s\n", secretString)
-
-	c.IndentedJSON(http.StatusOK, secretString)
 }
 
 func returnOK(c *gin.Context) {
@@ -47,16 +28,8 @@ func returnOK(c *gin.Context) {
 }
 
 func main() {
-
-	region := "us-east-1"
-
-	config, err := config.LoadDefaultConfig(context.TODO(), config.WithRegion(region))
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// Create Secrets Manager client
-	svc = secretsmanager.NewFromConfig(config)
+	// Create Secrets Manager cache
+	secretCache, _ = secretcache.New()
 
 	router := gin.Default()
 	router.GET("/secretsmanager/get", getSecret)
